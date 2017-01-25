f2ccb7cd16f6f15c350a33241398210e350c67fe
kothapan <nageswararao.kothapalli@cdk.com>
2017-01-25 18:08:44 +0530

    first commit

---
+++ DiceMain/DiceMainSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Policy;
+using Dice.DicePages;
+using Dice.DiceUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Dice
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static IWebDriver driver = TestSetUp.BringMyDriver();
+        DiceHomePage myDiceHomePage = new DiceHomePage(driver);
+
+        [TestInitialize]
+        public void LaunchApplication()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["AppURL"]);
+            
+        }
+        [TestMethod]
+        public void DiceMainSearch()
+        {
+            XLUtility.WriteCellValue("BooleanSearch",0,1,"MyNewColum");
+            myDiceHomePage.DiceSearch("Java","WA");
+        }
+
+        [TestMethod]
+        public void DiceBooleanSearch()
+        {
+            myDiceHomePage.BooleanSearch();
+           // myDiceHomePage.CaptureTheSearchResultsCount();
+        }
+    }
+}
